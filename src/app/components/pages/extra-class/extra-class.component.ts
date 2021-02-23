5c89fd9f7996915356ca69577d5204659508f842
Moses Nkrumah <mosessccss@gmail.com>
2021-02-23 01:43:55 +0000

    extra class on the way

--- src/app/components/pages/extra-class/extra-class.component.ts
+++ src/app/components/pages/extra-class/extra-class.component.ts
@@ -1,10 +1,29 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import '../../../../jitsi/external_api.js';
 
+declare var JitsiMeetExternalAPI: any;
 @Component({
   selector: 'app-extra-class',
   templateUrl: './extra-class.component.html',
   styleUrls: ['./extra-class.component.css'],
 })
 export class ExtraClassComponent implements OnInit {
+  title = 'app';
+  domain: string = 'meet.jit.si';
+  options: any;
+  api: any;
+
+  constructor() {}
+
+  ngAfterViewInit(): void {
+    this.options = {
+      roomName: 'JitsiMeetAPIExample',
+      width: 1000,
+      height: 1000,
+      parentNode: document.querySelector('#meet'),
+    };
+
+    this.api = new JitsiMeetExternalAPI(this.domain, this.options);
+  }
   ngOnInit(): void {}
 }
